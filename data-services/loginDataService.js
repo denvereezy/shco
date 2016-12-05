a37e5b54332f2683205836aec0a13cab46df2b49
denvereezy <denver@projectcodex.co>
2016-12-05 12:01:22 +0200

    login data service

---
+++ data-services/loginDataService.js
@@ -0,0 +1,9 @@
+const QueryDataService = require('./queryDataService');
+const encryptonator = require('encryptonator');
+
+module.exports = function(connection) {
+    const queryDataService = new QueryDataService(connection);
+    this.login = function(username) {
+          return queryDataService.executeQuery('select * from users where username = ?', username);
+    };
+};
