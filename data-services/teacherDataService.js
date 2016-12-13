415fbe07f9aa4f22296470372249f04ed30cc467
denvereezy <denver@projectcodex.co>
2016-12-13 08:25:45 +0200

    updated

--- data-services/teacherDataService.js
+++ data-services/teacherDataService.js
@@ -6,4 +6,16 @@ module.exports = function(connection) {
     this.add = function(data) {
         return queryDataService.executeQuery('insert into teachers set ?', data);
     };
+
+    this.receivedPayments = function(identifier) {
+        return queryDataService.executeQuery(`select payments.id,
+                                              month, DATE_FORMAT(payment_date,"%d %b %y") as payment_date,
+                                                amount, status, teacher_id, name, username, identifier from
+                                                 payments inner join teachers on payments.teacher_id = teachers.id
+                                                  inner join users on teachers.name = users.username where identifier = ?`, identifier);
+    };
+
+    this.acceptPayment = function(status, id) {
+        return queryDataService.executeQuery('update payments set status = ? where id = ?', [status, id]);
+    };
 };
