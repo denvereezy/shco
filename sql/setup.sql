6868c935ec9b3906a1b723e6595a3f4369cede9b
denvereezy <denver@projectcodex.co>
2016-12-14 07:20:21 +0200

    database setup

---
+++ sql/setup.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE shco;
+CREATE USER user@localhost IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON shco.* TO user@localhost;
+FLUSH PRIVILEGES;
+
+use shco;
+
+create table users (
+  id int not null primary key auto_increment,
+  username varchar(25) not null,
+  password varchar(100) not null,
+  role varchar(25) not null,
+  identifier varchar(25) not null
+);
+
+create table teachers (
+  id int not null primary key auto_increment,
+  name varchar(25) not null,
+  surname varchar(25) not null
+);
+
+create table payments (
+  id int not null primary key auto_increment,
+  month varchar(25) not null,
+  payment_date date not null,
+  amount int not null,
+  status varchar(25) not null,
+  teacher_id int not null,
+  foreign key(teacher_id) references teachers(id)
+);
+
+create table extras (
+  id int not null primary key auto_increment,
+  month varchar(25) not null,
+  payment_date date not null,
+  amount int not null,
+  comments varchar(100) not null
+);
