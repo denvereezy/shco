fa78e7bc058232a24798e423621f6d6f5799a37d
denver daniels <denver@projectcodex.co>
2016-12-30 07:22:36 +0200

    teachers

---
+++ routes/teachers.js
@@ -0,0 +1,39 @@
+const co = require('co');
+
+exports.receivedPayments = function(req, res, next) {
+    co(function*() {
+        try {
+            var identifier = req.session.identifier;
+            const services = yield req.getServices();
+            const teacherDataService = services.teacherDataService;
+            const receivedPayments = yield teacherDataService.receivedPayments(identifier);
+
+            res.render('receivedPayments', {
+                result: receivedPayments,
+                layout: 'teachers'
+            });
+        } catch (err) {
+            req.flash('alert', 'Error occurred');
+            res.redirect('/received-payments');
+            next(err);
+        };
+    });
+};
+
+exports.acceptPayment = function(req, res, next) {
+    co(function*() {
+        try {
+            var id = req.params.id,
+                status = 'confirmed';
+            const services = yield req.getServices();
+            const teacherDataService = services.teacherDataService;
+            const confirmPayment = yield teacherDataService.acceptPayment(status, id);
+            req.flash('success', 'Payment confirmed');
+            res.redirect('/received-payments');
+        } catch (err) {
+            req.flash('alert', 'Error occurred on accepting payment');
+            res.redirect('/received-payments');
+            next(err);
+        };
+    });
+};
