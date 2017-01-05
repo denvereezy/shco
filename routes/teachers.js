56505fbf9a95ad3dd8f8fb6e762e2d4a470647c8
denvereezy <denver@projectcodex.co>
2017-01-05 09:29:02 +0200

    showing username

--- routes/teachers.js
+++ routes/teachers.js
@@ -4,12 +4,14 @@ exports.receivedPayments = function(req, res, next) {
     co(function*() {
         try {
             var identifier = req.session.identifier;
+            var user = req.session.user;
             const services = yield req.getServices();
             const teacherDataService = services.teacherDataService;
             const receivedPayments = yield teacherDataService.receivedPayments(identifier);
 
             res.render('receivedPayments', {
                 result: receivedPayments,
+                user: user,
                 layout: 'teachers'
             });
         } catch (err) {
