7665dd1a10f3ceb2467282c4adba2ddf7d7d6291
denvereezy <denver@projectcodex.co>
2017-01-10 09:50:32 +0200

    updated

--- data-services/paymentDataService.js
+++ data-services/paymentDataService.js
@@ -12,11 +12,17 @@ module.exports = function(connection) {
     };
 
     this.edit = function(id) {
-        return queryDataService.executeQuery('select payments.id, name, month, status, payment_date, amount, teachers.id as teacher_id from payments inner join teachers on payments.teacher_id = teachers.id where payments.id = ?', id);
+        return queryDataService.executeQuery(`select payments.id, name, month, status,
+                                                DATE_FORMAT(payment_date, "%Y-%m-%d") as payment_date,
+                                                 amount, teachers.id as teacher_id from payments inner join
+                                                  teachers on payments.teacher_id = teachers.id where payments.id = ?`, id);
     };
 
     this.showPayments = function() {
-        return queryDataService.executeQuery('select payments.id, name, amount, month, status, DATE_FORMAT(payment_date,"%d %b %y") as payment_date from payments inner join teachers on payments.teacher_id = teachers.id order by payments.id desc');
+        return queryDataService.executeQuery(`select payments.id, name, amount,
+                                                month, status, DATE_FORMAT(payment_date,"%d %b %y") as payment_date
+                                                 from payments inner join teachers on payments.teacher_id = teachers.id
+                                                  order by payments.id desc`);
     };
 
     this.show = function() {
@@ -32,10 +38,24 @@ module.exports = function(connection) {
     };
 
     this.showOtherPayments = function() {
-        return queryDataService.executeQuery('select id, DATE_FORMAT(payment_date,"%d %b %y") as payment_date, amount, comments from extras');
+        return queryDataService.executeQuery(`select id, DATE_FORMAT(payment_date,"%d %b %y") as payment_date,
+                                                amount, comments from extras`);
     };
 
     this.otherPayments = function(data) {
         return queryDataService.executeQuery('insert into extras set ?', data);
+    };
+
+    this.editOtherPayment = function(id) {
+        return queryDataService.executeQuery(`select id, DATE_FORMAT(payment_date, "%Y-%m-%d") as payment_date,
+                                                amount, comments from extras where id = ?`, id);
+    };
+
+    this.updateOtherPayment = function(data, id) {
+        return queryDataService.executeQuery('update extras set ? where id = ?', [data, id]);
+    };
+
+    this.deleteOtherPayment = function(id) {
+        return queryDataService.executeQuery('delete from extras where id = ?', id);
     };
 };
