f7177b81c408ae9673b159658152ab1231bf11f5
denvereezy <denver@projectcodex.co>
2017-02-02 07:50:17 +0200

    attendance records

---
+++ routes/attendance.js
@@ -0,0 +1,41 @@
+const co = require('co');
+
+exports.takeAttendance = function(req, res, next) {
+    co(function * () {
+        try {
+            var data = {
+                student_id: req.body.student_id,
+                lesson: req.body.lesson
+            };
+            const services = yield req.getServices();
+            const attendanceDataService = services.attendanceDataService;
+            const result = yield attendanceDataService.takeAttendance(data);
+            res.redirect('attendance');
+        } catch (err) {
+            next(err);
+        }
+    });
+};
+
+exports.getAttendance = function(req, res, next) {
+    co(function * () {
+        try {
+            var user = req.session.user;
+            const services = yield req.getServices();
+            const attendanceDataService = services.attendanceDataService;
+            const studentDataService = services.studentDataService;
+            const students = studentDataService.getStudents();
+            const attendance = attendanceDataService.getAttendance();
+            const result = yield[students,
+                attendance];
+            res.render('attendance', {
+                students: result[0],
+                attendance: result[1],
+                user:user,
+                layout: 'teachers'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+};
