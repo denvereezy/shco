9d444f7b1851a56f634646962826f52024862682
denvereezy <denver@projectcodex.co>
2017-02-25 20:00:00 +0200

    update service

--- data-services/attendanceDataService.js
+++ data-services/attendanceDataService.js
@@ -10,4 +10,16 @@ module.exports = function(connection) {
     this.getAttendance = function(){
       return queryDataService.executeQuery('select attendance.id, student_id, DATE_FORMAT(lesson,"%d %b %y") as lesson, name from attendance inner join students on student_id = students.id');
     };
+
+    this.edit = function(id) {
+        return queryDataService.executeQuery('select * from attendance where id = ?', id);
+    };
+
+    this.update = function(data, id) {
+        return queryDataService.executeQuery('update attendance set ? where id = ?', [data, id]);
+    };
+
+    this.delete = function(id) {
+        return queryDataService.executeQuery('delete from attendance where id = ?', id);
+    };
 };
