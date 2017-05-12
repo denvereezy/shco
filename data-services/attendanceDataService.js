4f675bd62a48de04ace444d92765274b17eeadc1
denvereezy <denver@projectcodex.co>
2017-05-12 14:03:57 +0200

    updated service

--- data-services/attendanceDataService.js
+++ data-services/attendanceDataService.js
@@ -3,10 +3,6 @@ const QueryDataService = require('./queryDataService');
 module.exports = function(connection) {
     const queryDataService = new QueryDataService(connection);
 
-    this.takeAttendance = function(data) {
-        return queryDataService.executeQuery('insert into attendance set ?', data);
-    };
-
     this.getAttendance = function(){
       return queryDataService.executeQuery(`
         select attendance.id, student_id, DATE_FORMAT(lesson,"%d %b %y") as lesson,
@@ -16,17 +12,5 @@ module.exports = function(connection) {
                 inner join subjects
                   on subject_id = subjects.id`
       );
-    };
-
-    this.edit = function(id) {
-        return queryDataService.executeQuery('select * from attendance where id = ?', id);
-    };
-
-    this.update = function(data, id) {
-        return queryDataService.executeQuery('update attendance set ? where id = ?', [data, id]);
-    };
-
-    this.delete = function(id) {
-        return queryDataService.executeQuery('delete from attendance where id = ?', id);
     };
 };
