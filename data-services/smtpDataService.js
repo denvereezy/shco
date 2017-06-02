5f701fb747e981db473155af7fad0e89ce629efa
denvereezy <denver@projectcodex.co>
2017-06-02 14:00:45 +0200

    only using smtp config

--- data-services/smtpDataService.js
+++ data-services/smtpDataService.js
@@ -3,47 +3,33 @@ const nodemailer = require('nodemailer'),
 
 module.exports = function() {
     var smtpConfig = {
-        host: process.env.host,
+        service: process.env.host,
         port: 465,
         secure: true,
         auth: {
             user: process.env.user,
-            pass: process.env.password
+            pass: process.env.pass
         }
-    };
-
-    var poolConfig = {
-        pool: true,
-        host: process.env.host,
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.user,
-            pass: process.env.password
-        }
-    };
-
-    var directConfig = {
-        name: 'shco.com'
     };
 
     this.sendPasswordReset = function(data) {
 
-        var transporter = nodemailer.createTransport(smtpConfig, poolConfig, directConfig);
+        var transporter = nodemailer.createTransport('SMTP', smtpConfig);
+
         var mailOptions = {
             from: '"SHCO" <noreply@shco.com>',
-            to: [data.email],
+            to: data.email,
             subject: 'Password reset',
-            text: 'Please follow the link to reset your password. ' + process.env.link + data.token
+            text: `Please follow the link to reset your password. ${process.env.link}${data.token}`
         };
 
         transporter.sendMail(mailOptions, function(error, info) {
             if (error) {
-                console.log(error);
+                console.log('Error: ', error);
                 return error;
             } else {
-                console.log('Message sent: ' + info.response);
-                return 'Message sent: ' + info.response;
+                console.log('Message sent: ' + info.message);
+                return 'Message sent: ' + info.message;
             };
         });
     };
